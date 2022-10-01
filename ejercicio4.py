8c215b383cdb748853c88240b95f45173d40bcbc
Daniela Tassara Botello <d.tassarabotello@gmail.com>
2022-10-01 16:43:23 -0300

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,4 @@
+precio= int (input( "ingrese el precio del producto"))
+cantidad= int (input("ingrese la cantidad de productos a llevar"))
+total= cantidad * precio
+print ("el total a pagar es : " , total)
