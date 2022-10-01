230442347c449bba3201d88d9c8a002c888173fd
Daniela Tassara Botello <d.tassarabotello@gmail.com>
2022-10-01 17:24:01 -0300

    Add files via upload

---
+++ ejercicio11.py
@@ -0,0 +1,12 @@
+num1=int(input("Ingrese el primer valor:"))
+num2=int(input("Ingrese el segundo valor:"))
+if num1>num2:
+    suma=num1+num2
+    print("La suma de los dos valores es:" , suma)
+    resta=num1-num2
+    print("La diferencia de los dos valores es:" , resta)
+else:
+    producto = num1*num2;
+    print("El producto de los dos valores es", producto)
+    division = num2//num1;
+    print("La división de los dos valores es", division)
