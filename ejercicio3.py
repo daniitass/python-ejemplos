4e978af307d7c049e966ca554371c4b192800212
Daniela Tassara Botello <d.tassarabotello@gmail.com>
2022-10-01 16:34:51 -0300

    Add files via upload
    ejercicio de suma de dos números por teclado y su producto.

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+num1= int(input("ingrese un valor"))
+num2=int(input("ingrese otro valor"))
+suma= num1 + num2
+producto = num1 * num2
+print("la suma de ambos numeros es: " , suma)
+print ("el producto de los numeros es: " , producto)
+
